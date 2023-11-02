a12ad217b7192ee5bdc38bcb468c57fcd8013b84
arasaix <xavi@DESKTOP-PMTH6V6>
2023-11-02 17:33:19 +0100

    day4 second part

--- 2022/day4/2/main.py
+++ 2022/day4/2/main.py
@@ -4,12 +4,21 @@ def main():
     file = open("../input.txt", "r")
     total = 0
     for line in file:
+        
         first_number = int(line[0:line.index("-")])
         second_number = int(line[(line.index("-")+1):line.index(",")])
         third_number = int(line[(line.index(",")+1):line.index("-", int(line.index(",")))])
         fourth_number = int(line[(line.index("-", int(line.index(","))))+1:-1])
-        if ((first_number >= third_number) and (second_number <= fourth_number)) or ((third_number >= first_number) and (fourth_number <= second_number)):
-            total = total + 1
+
+        if ((first_number == third_number) or (first_number == fourth_number)):
+            total+=1
+        elif ((second_number == third_number) or (second_number == fourth_number)):
+            total+=1
+        elif ((first_number > third_number) and (first_number < fourth_number)):
+            total+=1
+        elif ((third_number > first_number) and (third_number < second_number)):
+            total+=1
+
     print("El total es:",total)
 
 
